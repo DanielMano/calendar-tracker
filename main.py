8e88dad7a0525828dc06b7405c24b24c7cffaebc
DanielMano <mano.daniel@yahoo.com>
2023-02-06 19:02:12 -0600

    started getting calendar data
    removed excess row and added correct day of the month to each day tile
    DayActivityIcon can now have color of icon picked when being created if needed

--- main.py
+++ main.py
@@ -11,42 +11,47 @@ from kivy.properties import ListProperty, NumericProperty
 
 from ast import literal_eval
 
-
+import calendar_data
 #Builder.load_file('calendartracker.kv')
 
 class CalendarGrid(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.init_data()
                 
-        for row in range(7):
+        for row in range(6):
             for column in range(7):
-                #self.children[0].add_widget(self.add_crazy_button(j, i))
-                #self.children[0].add_widget(Label(text=f"({j}, {i})"))
-                self.children[0].add_widget(self.add_day_tile_layout(column, row))
+                daynum = self.active_month_array[row][column]
+                #TODO if daynum = 0, replace with correct days from either last or next month
+                self.children[0].add_widget(self.add_day_tile_layout(column, row, daynum))
+    
+    def init_data(self):
+        self.active_date = calendar_data.get_today_date()
+        self.active_month_array = calendar_data.get_month_data(self.active_date.year, self.active_date.month)
+        if len(self.active_month_array) < 6:
+            self.active_month_array.append([0, 0, 0, 0, 0, 0, 0])
+        self.month_names = calendar_data.get_month_names()
                 
-    def add_day_tile_layout(self, column, row):
+    def add_day_tile_layout(self, column, row, daynum):
         box_layout = BoxLayout(orientation='vertical')
-        box_layout.add_widget(Button(text=f"({column}, {row})"))
+        box_layout.add_widget(Button(text=f"{daynum}"))
         box_layout.add_widget(self.add_icon_grid_to_day_tile(column, row))
         return box_layout
     
     def add_icon_grid_to_day_tile(self, column, row):
         grid_layout = GridLayout(rows=2)
         for position in range(8):
-            grid_layout.add_widget(DayActivityIcon(text=f"({column}, {row}, {position})", font_size="10"))
+            grid_layout.add_widget(DayActivityIcon(column=column, row=row, position=position))
         return grid_layout
 
 
 class DayActivityIcon(Button):
     background_color = ListProperty((0, 0, 0, 1))
+    icon_color = ListProperty((0.2, 0.6, 1, 1))
     column = NumericProperty(0)
     row = NumericProperty(0)
     position = NumericProperty(0)
-    
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.column, self.row, self.position = literal_eval(self.text)
-        
+            
     def get_day_info(self):
         print(f"Pressed on day: {self.column}, {self.row}, {self.position}")
         pass
