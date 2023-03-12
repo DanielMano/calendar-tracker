cbc52ca859d1a683b25469eeb47b899905cbf45b
DanielMano <mano.daniel@yahoo.com>
2023-03-12 00:25:26 -0500

    Day Grid changed to handle display of event colors

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from kivy.properties import ListProperty
 
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDIconButton
 
 import calendar_data
 import database
@@ -20,7 +21,8 @@ import database
 class ScreenManager(ScreenManager):
     def init_data(self):
         # YYYY, M where [1-12], D
-        self.active_year, self.active_month, self.active_day = calendar_data.get_today_date_string()
+        self.today = calendar_data.get_today_date_string()
+        self.active_year, self.active_month, self.active_day = self.today
         # List of month names where 0 = '', 1 = 'January', and 12 = 'December'
         self.month_names = calendar_data.get_month_names()
 
@@ -85,11 +87,35 @@ class ScreenManager(ScreenManager):
     
     def add_day(self, day):
         btn = DayTile()
-        btn.ids.day.bind(on_press=self.day_press)
-        btn.ids.day.text = str(day)
-        # TODO: get data from database
+        btn.ids.day_tile.bind(on_press=self.day_press)
+
+        if day == self.today[2] and self.active_month == self.today[1] and self.active_year == self.today[0]:
+            # If today's date, set day selection, text color to white, and underline text
+            btn.ids.day_tile.line_color = cal_app.theme_cls.primary_light
+            btn.ids.day_tile.text = f"[u]{day}[/u]"
+            btn.ids.day_tile.text_color = "orange"
+        else:
+            btn.ids.day_tile.line_color = [0, 0, 0, 0]
+            btn.ids.day_tile.text = str(day)
+            btn.ids.day_tile.text_color = cal_app.theme_cls.primary_dark
         
+        # TODO: get data from database and 1. replace day_press popup with dialog and 2. add event icons for events
+
+        #btn.ids.event_grid.add_widget(self.add_event_icon([0.5, 0, 0.5, 1]))
+
         return btn
+    
+    def add_event_icon(self, color):
+        event_icon = MDIconButton(
+                theme_text_color="Custom",
+                md_bg_color_disabled = color,
+                pos_hint= {'center_x': 0.5,'center_y': 0.5},
+                icon_size = -12,
+                halign = 'center',
+                disabled = 'True',
+                )
+        return event_icon
+        
 
 class CalendarScreen(Screen):
     def prev_month(self):
@@ -103,19 +129,17 @@ class DayPopupLayout(MDBoxLayout):
 
 class DayTile(Widget):
     pass
-class DayIcon(Label):
-    event_color = ListProperty(())
-    pass
+
 class CalendarTrackerApp(MDApp):
     def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Teal"
+        
         # Create screen manager
         self.sm = ScreenManager()
         self.sm.init_data()
         # Create and display current month screen on startup
         self.sm.create_month_screen()
-        
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Teal"
 
         return self.sm
 
